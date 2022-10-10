f32b067115fd6afa88f78521613e6bae03d18a6a
Alícia Pereira <allypereira1@hotmail.com>
2022-10-10 22:58:10 +0100

    all finished

--- CsharpV8ConsoleApp2/Program.cs
+++ CsharpV8ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,12 +10,24 @@ namespace CsharpV8ConsoleApp2
     {
         static async Task Main(string[] args)
         {
-            var HtmlPage = await GetHtmlPage("https://www.youtube.com");
-            var FoundImg = ImgExtraction(HtmlPage);
+            var tasks = new List<Task>();
+            for(int i = 0; i < 8; i++)
+            { 
+             var task = Task.Run(async () =>
+              {
+                  var HtmlPage = await GetHtmlPage("https://www.youtube.com");
+                  var FdIm = ImgExtraction(HtmlPage);
+                  await SaveImages(FdIm);
+              });
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
             Console.WriteLine("...");
         }
         private static async Task<string> GetHtmlPage(string uri)
         {
+            Console.WriteLine("...");
             using (var httpClient = new HttpClient())
             {
                 var HtmlPage = await httpClient.GetStringAsync(uri);
@@ -25,7 +38,24 @@ namespace CsharpV8ConsoleApp2
         {
             var QueryImgRegex = new Regex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?\.jpg");
             var FoundImg = QueryImgRegex.Matches(HtmlPage);
-            return FoundImg.Select(x => x.Value).ToList();
+            var FdIm =  FoundImg.Select(X => X.Value).ToList();
+            return FdIm;
+        }
+        static async Task SaveImages(IList <string> imagesUtl)
+        {
+            var DirName = "img";
+            if (!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+            using (var httpClient = new HttpClient())
+            {
+              foreach(var UrlImage in imagesUtl) 
+                {
+                    Console.WriteLine($"Downloading Image: {UrlImage}");
+                    var ImgName = Path.Combine(DirName, $"{Guid.NewGuid()}.jpg");
+                    var img = await httpClient.GetByteArrayAsync(UrlImage);
+                     await File.WriteAllBytesAsync(ImgName, img);
+                }
+            }
         }
     }
 
