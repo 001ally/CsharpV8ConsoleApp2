c836d2b1ff216c97edb66f839bb9716a389bb8ca
Alícia Pereira <Alcia Pereira@ITGAOWK108>
2022-10-10 18:23:24 +0100

    Adicionar arquivos de projeto.

---
+++ CsharpV8ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CsharpV8ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var HtmlPage = await GetHtmlPage("https://www.youtube.com");
+            var FoundImg = ImgExtraction(HtmlPage);
+            Console.WriteLine("...");
+        }
+        private static async Task<string> GetHtmlPage(string uri)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var HtmlPage = await httpClient.GetStringAsync(uri);
+                return HtmlPage;
+            }
+        }
+        private static IList<string> ImgExtraction(string HtmlPage)
+        {
+            var QueryImgRegex = new Regex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?\.jpg");
+            var FoundImg = QueryImgRegex.Matches(HtmlPage);
+            return FoundImg.Select(x => x.Value).ToList();
+        }
+    }
+
+}
